1c1810146a2028d6c8d3c4d21d343c8a256c2495
SusieHatter <susiivaldez@gmail.com>
2021-07-24 20:38:45 -0500

    Host frontend with express.static (fixes styles.css link issues)
    Hide weather API key

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const https = require("https");
 const app = express();
+const { apiKey } = require("./config.js");
 
 app.use(express.json());
 app.use(
@@ -9,14 +10,11 @@ app.use(
   })
 );
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
+app.use("/", express.static("public"));
 
 app.post("/", function (req, res) {
   const query = req.body.cityName;
   const units = "metric";
-  const apiKey = "40dec95d7c9898c1fcc039795934a278";
   const url = `https://api.openweathermap.org/data/2.5/find?q=${query}&units=${units}&appid=${apiKey}`;
 
   https.get(url, function (response) {
@@ -26,9 +24,29 @@ app.post("/", function (req, res) {
       const description = weatherData.list[0].weather[0].description;
       const icon = weatherData.list[0].weather[0].icon;
       const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-      res.send(`<h1>The temperature in ${query} is ${temp} degrees Celsius.</h1>
-      <h3>The weather is currently ${description}</h3>
-      <img src="${iconUrl}" alt="icon">`);
+      res.send(`
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Noto+Sans&family=Suez+One&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="assets/styles.css" />
+    <title>Weather App</title>
+  </head>
+  <body>
+    <h1>The temperature in ${query} is ${temp} degrees Celsius.</h1>
+    <h3>The weather is currently ${description}</h3>
+    <img src="${iconUrl}" alt="icon">
+  </body>
+</html>
+      `);
     });
   });
 });
